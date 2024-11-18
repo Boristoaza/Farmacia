0621b0b980d9eac4ab3e19de4881a785c587d8cd
Boristoaza <boristoaza7@gmail.com>
2024-11-18 12:21:30 -0500

    ruta arreglada

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\InicioController;
 use App\Http\Controllers\InventarioDatosController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+     return view('welcome');
+ });
 Route::get('/index',[InicioController::class , 'index'])->name('index');
 Route::get('/inventario', [inventarioDatosController::class, 'inventario']);
